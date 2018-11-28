f01bc8db4735a70eeb1f29ad6ae8c7b5466cc75e
Sergei Cherniaev <sergei@cherniaev.com>
2018-11-28 11:33:49 +0300

    merge sort recusive implementation

--- merge-sort.js
+++ merge-sort.js
@@ -1,5 +1,36 @@
 function mergeSort(arr) {
+  if (arr.length > 1) {
+    const pivotPosition = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, pivotPosition));
+    const right = mergeSort(arr.slice(pivotPosition));
+    return mergeArr(left, right);
+  }
   return arr;
+}
+
+function mergeArr(arr1, arr2) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+      continue;
+    }
+    merged.push(arr2[j]);
+    j++;
+  }
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+
+  return merged;
 }
 
 module.exports = mergeSort;
