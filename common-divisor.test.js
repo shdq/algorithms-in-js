4524a2956aeb603e86b07c740e36337d24273f95
Sergei Cherniaev <sergei@cherniaev.com>
2018-12-17 11:14:57 +0300

    test suit updated

--- common-divisor.test.js
+++ common-divisor.test.js
@@ -2,6 +2,6 @@ const divisor = require('./common-divisor');
 
 /* eslint max-len: 0 */
 test('returns the greatest common divisor of two given numbers', () => {
-  expect(divisor(10, 20))
-      .toEqual(10);
+  expect(divisor(8, 20))
+      .toEqual(4);
 });
