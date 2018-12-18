1d78d81e2bd6fcdb8039f0e3611327aeec8662d9
Sergei Cherniaev <sergei@cherniaev.com>
2018-12-18 17:33:46 +0300

    graph representation as adjacency list added

--- graph.js
+++ graph.js
@@ -2,6 +2,14 @@ class Graph {
   constructor(size) {
     this.name = 'Graph';
     this.size = size;
+    this.adjList = new Map();
+    for (let i = 1; i <= size; i++) {
+      this.adjList.set(i, new Set());
+    }
+  }
+  addEdge(from, to) {
+    this.adjList.get(from).add(to);
+    this.adjList.get(to).add(from);
   }
 }
 
