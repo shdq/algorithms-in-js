36f5ca8a2303f87976547b13c04dcb0dd8f49ba0
Sergei Cherniaev <sergei@cherniaev.com>
2018-12-20 15:29:07 +0300

    test for dublicate edges added

--- graph.test.js
+++ graph.test.js
@@ -13,8 +13,6 @@ test('returns a map of adjacency lists for each vertex', () => {
   g.addEdge(3, 4);
   g.addEdge(3, 5);
   g.addEdge(4, 5);
-  g.addEdge(4, 5);
-  g.addEdge(4, 5);
 
   const expected = new Map([
     [1, new Set([2])],
@@ -22,6 +20,22 @@ test('returns a map of adjacency lists for each vertex', () => {
     [3, new Set([2, 4, 5])],
     [4, new Set([3, 5])],
     [5, new Set([3, 4])],
+  ]);
+
+  expect(g.adjList).toEqual(expected);
+});
+
+test('no dublicate edges in returned graph', () => {
+  const g = new Graph(3);
+  g.addEdge(1, 2);
+  g.addEdge(2, 3);
+  g.addEdge(2, 3);
+  g.addEdge(2, 3);
+
+  const expected = new Map([
+    [1, new Set([2])],
+    [2, new Set([1, 3])],
+    [3, new Set([2])],
   ]);
 
   expect(g.adjList).toEqual(expected);
