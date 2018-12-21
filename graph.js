77fd84f62c1898116711e4b6e882b77387befed0
Sergei Cherniaev <sergei@cherniaev.com>
2018-12-21 16:40:00 +0300

    code formatted

--- graph.js
+++ graph.js
@@ -7,10 +7,12 @@ class Graph {
       this.adjList.set(i, new Set());
     }
   }
+
   addEdge(from, to) {
     this.adjList.get(from).add(to);
     this.adjList.get(to).add(from);
   }
+
   print() {
     for (const [vertex, list] of this.adjList) {
       console.log(`Vertex ${vertex} adjacency list: ${[...list]}`);
