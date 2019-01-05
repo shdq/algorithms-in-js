9a55b841c88887a61e2f7001bfcef2447c520f29
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-05 23:09:12 +0300

    problem 1 solved

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+function multiples(n) {
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+module.exports = multiples;
