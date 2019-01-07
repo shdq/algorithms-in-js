59e627128347fe4d09e2e762482f3ac54183864d
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-07 22:33:08 +0300

    test for fibonacci sequence added

---
+++ problem-2.test.js
@@ -0,0 +1,7 @@
+const fib = require('./problem-2');
+
+/* eslint max-len: 0 */
+test('returned fibonacci sequence that lower than a given number', () => {
+  expect(fib.fibonacci(100))
+    .toEqual([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
+});
