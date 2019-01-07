16cd2ba7c07558a95f0ce7509849e0ad8b5e1176
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-07 22:38:19 +0300

    test for sum of fibonacci sequence

--- problem-2.test.js
+++ problem-2.test.js
@@ -5,3 +5,8 @@ test('returned fibonacci sequence that lower than a given number', () => {
   expect(fib.fibonacci(100))
     .toEqual([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
 });
+
+test('returned sum of even fibonacci sequence', () => {
+  expect(fib.fiboSum(fib.fibonacci(100)))
+    .toBe(44);
+});
