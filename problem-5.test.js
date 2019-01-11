4c805c46b6cf582925c213549226f29244088e82
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-11 14:18:51 +0300

    test for problem 5

---
+++ problem-5.test.js
@@ -0,0 +1,6 @@
+const multiple = require('./problem-5');
+
+/* eslint max-len: 0 */
+test(`returns smallest number that can be divided by each of the numbers from 1 to 10 without any remainder`, () => {
+  expect(multiple(10)).toBe(2520);
+});
