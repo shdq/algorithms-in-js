7f8178711db08ed90f8c0cb856985c74b2ed810b
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-14 11:20:42 +0300

    tests for the square of the sum and square difference added

--- problem-6.test.js
+++ problem-6.test.js
@@ -6,6 +6,11 @@ test(`returns the sum of the squares of the first N natural numbers`, () => {
 });
 
 /* eslint max-len: 0 */
+test(`returns the square of the sum of the first N natural numbers`, () => {
+  expect(problem.squareSum(10)).toBe(3025);
+});
+
+/* eslint max-len: 0 */
 test(`returns difference between the sum of the squares of the first ten natural numbers and the square of the sum`, () => {
   expect(problem.sqrDiff(10)).toBe(2640);
 });
