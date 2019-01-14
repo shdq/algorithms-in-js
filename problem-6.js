46ddb324b4e13ffb7bb5e35dc99d2d0e836a65f3
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-14 11:09:30 +0300

    the sum of the squares function added

--- problem-6.js
+++ problem-6.js
@@ -1,5 +1,14 @@
+function sumSquare(n) {
+  let result = 0;
+  result += n * n;
+  if (n > 1) return (result += sumSquare(n - 1));
+  return result;
+}
+
 function sqrDiff(n) {
   return n;
 }
 
-module.exports = sqrDiff;
+console.log(sumSquare(10));
+
+module.exports = { sumSquare, sqrDiff };
