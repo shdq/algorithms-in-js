c9dc1d5e0ccf1642aeee63242e0dee16745c94f9
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-14 11:08:23 +0300

    test for the sum of squares

--- problem-6.test.js
+++ problem-6.test.js
@@ -1,6 +1,11 @@
-const sqrDiff = require("./problem-6");
+const problem = require("./problem-6");
+
+/* eslint max-len: 0 */
+test(`returns the sum of the squares of the first N natural numbers`, () => {
+  expect(problem.sumSquare(10)).toBe(385);
+});
 
 /* eslint max-len: 0 */
 test(`returns difference between the sum of the squares of the first ten natural numbers and the square of the sum`, () => {
-  expect(sqrDiff(10)).toBe(2640);
+  expect(problem.sqrDiff(10)).toBe(2640);
 });
