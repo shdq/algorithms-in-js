ad50b521bd02f75eac1c8c26249f102fa978fc11
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-17 16:29:44 +0300

    tests for isPrime

--- problem-7.test.js
+++ problem-7.test.js
@@ -2,5 +2,26 @@ const problem = require("./problem-7");
 
 /* eslint max-len: 0 */
 test("checks number is prime or not", () => {
+  expect(problem.isPrime(1)).toBe(false);
+});
+test("checks number is prime or not", () => {
+  expect(problem.isPrime(2)).toBe(true);
+});
+test("checks number is prime or not", () => {
+  expect(problem.isPrime(3)).toBe(true);
+});
+test("checks number is prime or not", () => {
+  expect(problem.isPrime(4)).toBe(false);
+});
+test("checks number is prime or not", () => {
+  expect(problem.isPrime(5)).toBe(true);
+});
+test("checks number is prime or not", () => {
+  expect(problem.isPrime(6)).toBe(false);
+});
+test("checks number is prime or not", () => {
+  expect(problem.isPrime(11)).toBe(true);
+});
+test("checks number is prime or not", () => {
   expect(problem.isPrime(13)).toBe(true);
 });
