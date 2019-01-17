df6bf897027f6d7432884862266948ae95b9d62b
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-17 17:04:41 +0300

    isPrime fixed

--- problem-7.js
+++ problem-7.js
@@ -1,10 +1,14 @@
 function isPrime(n) {
   if (n === 2 || n === 3) return true;
   if (n < 2 || n % 2 === 0) return false;
-  for (let i = 3; i < Math.floor(Math.sqrt(n)); i + 2) {
+  for (let i = 3; i <= Math.floor(Math.sqrt(n)); i += 2) {
     if (n % i === 0) return false;
   }
   return true;
 }
 
-module.exports = { isPrime };
+function nthPrime(n) {
+  return n;
+}
+
+module.exports = { isPrime, nthPrime };
