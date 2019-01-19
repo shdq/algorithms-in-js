693d7a27db00a3509a8ee619dbcf6efc77dc4c15
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-19 09:48:48 +0300

    test for problem 8 added

---
+++ problem-8.test.js
@@ -0,0 +1,6 @@
+const largest = require("./problem-8");
+
+/* eslint max-len: 0 */
+test("return largest product of N numbers in the 1000-digit number", () => {
+  expect(largest(4)).toBe(5832);
+});
