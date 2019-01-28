318b62e7a03410d0b0b3dd759fda325282dfb6ef
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-28 22:17:25 +0300

    tests description added

--- problem-1.test.js
+++ problem-1.test.js
@@ -1,8 +1,10 @@
-const multiplies = require('./problem-1');
+const multiplies = require("./problem-1");
 
-/* eslint max-len: 0 */
-test('should be return sum of all multiples of 3 and 5', () => {
-  expect(multiplies(10)).toEqual(23);
+describe("Tests for problem 1: Multiples of 3 and 5", () => {
+  /* eslint max-len: 0 */
+  test("should be return sum of all multiples of 3 and 5", () => {
+    expect(multiplies(10)).toEqual(23);
 
-  expect(multiplies(1000)).toEqual(233168);
+    expect(multiplies(1000)).toEqual(233168);
+  });
 });
