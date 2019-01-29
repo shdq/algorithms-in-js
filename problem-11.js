27af358b09e26ad9bc9cb40e3c0d1afbfd3f2fa6
Sergei Cherniaev <sergei@cherniaev.com>
2019-01-29 20:24:38 +0300

    class matrix added

--- problem-11.js
+++ problem-11.js
@@ -1,5 +1,10 @@
-function greatestProduct() {
-  return;
+class Matrix {
+  constructor(width = 5, height = 5) {
+    this.name = "Matrix";
+    this.width = width;
+    this.height = height;
+    this.data = [];
+  }
 }
 
-module.exports = greatestProduct;
+module.exports = Matrix;
