1dddd9b5edb679bc19fde5ed26498fc1491f8122
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-04 16:01:20 +0300

    test for an amount of number's factors function added

--- problem-12.test.js
+++ problem-12.test.js
@@ -1,8 +1,12 @@
-const { triangleNum } = require("./problem-12");
+const { triangleNum, factors } = require("./problem-12");
 
 describe("Tests for problem 12: Highly divisible triangular number", () => {
   /* eslint max-len: 0 */
   test("returns Nth triangle number", () => {
     expect(triangleNum(6)).toBe(21);
+  });
+
+  test("returns amount of factors of a given number", () => {
+    expect(factors(21)).toBe(4);
   });
 });
