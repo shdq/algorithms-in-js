a05a54038178e4f9874c19d4cd3e64179f3363bb
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-04 16:02:36 +0300

    amount of number's factors function added

--- problem-12.js
+++ problem-12.js
@@ -5,4 +5,14 @@ function triangleNum(n) {
   return 1;
 }
 
-module.exports = { triangleNum };
+function factors(n) {
+  const factors = [1];
+  for (let i = 2; i <= n; i++) {
+    if (n % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors.length;
+}
+
+module.exports = { triangleNum, factors };
