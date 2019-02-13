e1d00e00e8300d35e1985806077455091bac5d4a
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-13 18:03:44 +0300

    solution refactored

--- problem-13.js
+++ problem-13.js
@@ -1,20 +1,18 @@
 function largeSum(n) {
   const arr = n.split("\n");
-  const result = [];
+  let result = 0;
   let size;
   arr[0].length < 10 ? (size = arr[0].length - 1) : (size = 10);
   let i = 0;
-  while (result.length <= size) {
-    let temp = 0;
-    temp = arr.reduce((sum, val) => sum + Number(val[i]), 0);
+  while (i <= size) {
+    const rowSum = arr.reduce((sum, val) => sum + Number(val[i]), 0);
     const exp = size - i;
-    result.push(temp * Math.pow(10, exp));
+    result += rowSum * Math.pow(10, exp);
     i++;
   }
-  const sum = result.reduce((sum, val) => sum + val);
-  const str = sum.toString();
+  const str = result.toString();
   if (str.length > 10) return Number(str.slice(0, 10));
-  return sum;
+  return result;
 }
 
 module.exports = largeSum;
