4b2e2c4cff992e62103c4bce62d5d7b6eae3bb40
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-19 12:14:20 +0300

    Collatz sequence length function added

--- problem-14.js
+++ problem-14.js
@@ -1,5 +1,10 @@
-function sequence() {
-  return 0;
+function sequence(n, length = 0) {
+  length++;
+  if (n === 1) return length;
+  if (n % 2 === 0) {
+    return sequence(n / 2, length);
+  }
+  return sequence(3 * n + 1, length);
 }
 
 module.exports = sequence;
--- problem-14.test.js
+++ problem-14.test.js
@@ -2,7 +2,7 @@ const sequence = require("./problem-14");
 
 describe("Tests for problem 14: Longest Collatz sequence", () => {
   /* eslint max-len: 0 */
-  test("returns a number", () => {
-    expect(sequence()).toBe(0);
+  test("returns a length of a sequence", () => {
+    expect(sequence(13)).toBe(10);
   });
 });
