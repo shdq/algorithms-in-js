030f062d48670ac809487ac9779e0473382b87a7
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-19 12:55:02 +0300

    straight-forward solution for the largest Collatz sequence below one million

--- problem-14.js
+++ problem-14.js
@@ -7,4 +7,17 @@ function sequence(n, length = 0) {
   return sequence(3 * n + 1, length);
 }
 
-module.exports = sequence;
+function longestSeq(n = 1000000) {
+  let num = 0;
+  let longest = 0;
+  for (; n >= 2; n--) {
+    const length = sequence(n);
+    if (longest < length) {
+      longest = length;
+      num = n;
+    }
+  }
+  return num;
+}
+
+module.exports = { sequence, longestSeq };
