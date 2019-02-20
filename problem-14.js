bfac125325f0f85ffd0a2498ed1f155056fe2b64
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-20 11:14:08 +0300

    solution optimised and became more than 20 times faster

--- problem-14.js
+++ problem-14.js
@@ -1,20 +1,23 @@
-function sequence(n, length = 0) {
+function sequence(n, length = 0, dataMap) {
+  if (dataMap && dataMap.has(n)) return dataMap.get(n) + length;
   length++;
   if (n === 1) return length;
   if (n % 2 === 0) {
-    return sequence(n / 2, length);
+    return sequence(n / 2, length, dataMap);
   }
-  return sequence(3 * n + 1, length);
+  return sequence(3 * n + 1, length, dataMap);
 }
 
 function longestSeq(n = 1000000) {
+  const dataMap = new Map();
   let num = 0;
   let longest = 0;
-  for (; n >= 2; n--) {
-    const length = sequence(n);
+  for (let i = 2; i <= n; i++) {
+    const length = sequence(i, 0, dataMap);
+    dataMap.set(i, length);
     if (longest < length) {
       longest = length;
-      num = n;
+      num = i;
     }
   }
   return num;
