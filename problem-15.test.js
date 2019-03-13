55458d71c9e2e86f392be9a9f5a986f1a0c15bb4
Sergei Cherniaev <sergei@cherniaev.com>
2019-03-13 17:27:38 +0300

    function renamed

--- problem-15.test.js
+++ problem-15.test.js
@@ -1,8 +1,8 @@
-const routes = require("./problem-15");
+const paths = require("./problem-15");
 
 describe("Tests for problem 15: Lattice paths", () => {
   /* eslint max-len: 0 */
   test("returns number of routes for a given grid size", () => {
-    expect(routes(2)).toBe(6);
+    expect(paths(2)).toBe(6);
   });
 });
