68571e2f6861482fc71e98b5b7cb23b11a817837
Sergei Cherniaev <sergei@cherniaev.com>
2019-03-14 17:04:23 +0300

    problem 15 solved in combinatorial way, test adjusted

--- problem-15.test.js
+++ problem-15.test.js
@@ -2,7 +2,7 @@ const paths = require("./problem-15");
 
 describe("Tests for problem 15: Lattice paths", () => {
   /* eslint max-len: 0 */
-  test("returns number of routes for a given grid size", () => {
-    expect(paths(2)).toBe(6);
+  test("returns number of paths for a given grid size", () => {
+    expect(paths(20)).toBe(137846528820);
   });
 });
