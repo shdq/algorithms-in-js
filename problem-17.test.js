6868b02b8c434ec79cefe86814c7d2aa2bf38b3a
Sergei Cherniaev <sergei@cherniaev.com>
2019-03-24 18:38:50 +0300

    test for the problem 17

---
+++ problem-17.test.js
@@ -0,0 +1,8 @@
+const countLetters = require("./problem-17");
+
+describe("Tests for problem 17: Number letter counts", () => {
+  /* eslint max-len: 0 */
+  test("returns number of letters in written words from 1 to 5", () => {
+    expect(countLetters(5)).toBe(19);
+  });
+});
