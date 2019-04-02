86df61c6fcbffa718cfbaef3809faf7ff36e9832
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-02 15:48:23 +0300

    one more test for edge case

--- problem-17.test.js
+++ problem-17.test.js
@@ -5,4 +5,7 @@ describe("Tests for problem 17: Number letter counts", () => {
   test("returns number of letters in written words from 1 to 5", () => {
     expect(countLetters(5)).toBe(19);
   });
+  test("returns number of letters in written words from 1 to 1000", () => {
+    expect(countLetters(1000)).toBe(21124);
+  });
 });
