a6ea8e15d90636337c8893b2963380d31e7af9e5
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-08 16:30:14 +0300

    test fixed according to the problem

--- problem-19.test.js
+++ problem-19.test.js
@@ -2,7 +2,7 @@ const countSundays = require("./problem-19");
 
 describe("Tests for problem 19: Counting Sundays", () => {
   /* eslint max-len: 0 */
-  test("returns number of Sundays between two dates", () => {
-    expect(countSundays("2019-01-01", "2019-03-31")).toBe(13);
+  test("returns number of Sundays that fell on the first of the month between two dates", () => {
+    expect(countSundays("1901-01-01", "2000-12-31")).toBe(171);
   });
 });
