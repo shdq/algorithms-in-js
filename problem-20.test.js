4b62d6b3f8c936ceca5b915946086a1349c17156
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-10 12:05:08 +0300

    test for a big number added

--- problem-20.test.js
+++ problem-20.test.js
@@ -5,4 +5,7 @@ describe("Tests for problem 20: Factorial digit sum", () => {
   test("returns sum of the digits in the factorial of a given number", () => {
     expect(digitSum(10)).toBe(27);
   });
+  test("returns sum of the digits in the factorial of a given big number", () => {
+    expect(digitSum(100)).toBe(648);
+  });
 });
