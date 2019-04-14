9fe5b78a589979e0f8653b36d80e382adcd6315d
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-14 20:46:00 +0300

    tests for amicable function added

--- problem-21.test.js
+++ problem-21.test.js
@@ -1,8 +1,17 @@
-const { divisorsSum } = require("./problem-21");
+const { divisorsSum, amicable } = require("./problem-21");
 
 describe("Tests for problem 21: Amicable numbers", () => {
   /* eslint max-len: 0 */
   test("returns sum of the proper divisors of a given number", () => {
     expect(divisorsSum(220)).toBe(284);
   });
+
+  test("returns amicable number a given number", () => {
+    expect(amicable(284)).toBe(220);
+  });
+
+  test("returns false is there is no amicable number a given number", () => {
+    expect(amicable(500)).toBe(false);
+  });
+
 });
