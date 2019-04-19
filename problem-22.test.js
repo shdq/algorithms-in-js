63b44244abddb9d51d3855862f38e0e0609f8172
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-19 11:11:37 +0300

    test for the total score of names list in the file

--- problem-22.test.js
+++ problem-22.test.js
@@ -1,8 +1,15 @@
-const { alphaValue } = require("./problem-22");
+const fs = require("fs");
+const names = fs.readFileSync("problem-22-data.txt", "utf-8");
+
+const { alphaValue, totalScore } = require("./problem-22");
 
 describe("Tests for problem 22: Names scores", () => {
   /* eslint max-len: 0 */
   test("returns alphabetical value of a given word", () => {
     expect(alphaValue("COLIN")).toBe(53);
+  });
+
+  test("returns total score of all names in the list", () => {
+    expect(totalScore(names)).toBe(871198282);
   });
 });
