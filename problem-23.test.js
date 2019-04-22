bbf57b86c15acad3eedfd355e5056e4f4384007f
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-22 17:14:42 +0300

    tests for isAbundant(number) function

---
+++ problem-23.test.js
@@ -0,0 +1,16 @@
+const { isAbundant } = require("./problem-23");
+
+describe("Tests for problem 23: Non-abundant sums", () => {
+  /* eslint max-len: 0 */
+  test("checks the lowest abundant number for abundance", () => {
+    expect(isAbundant(12)).toBe(true);
+  });
+
+  test("checks perfect number for abundance", () => {
+    expect(isAbundant(28)).toBe(false);
+  });
+
+  test("checks number for abundance", () => {
+    expect(isAbundant(42)).toBe(true);
+  });
+});
