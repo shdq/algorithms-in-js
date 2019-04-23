76c7635b7e63685d852291bda46b32fba700b077
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-23 14:59:33 +0300

    problem 23 solved

--- problem-23.js
+++ problem-23.js
@@ -6,4 +6,34 @@ function isAbundant(n) {
   return false;
 }
 
-module.exports = { isAbundant };
+/*
+  all integers greater than 28123 can be written
+  as the sum of two abundant numbers
+*/
+const limit = 28123;
+
+function fillSet() {
+  const set = new Set();
+  for (let i = 1; i < limit; i++) {
+    if (isAbundant(i)) set.add(i);
+  }
+  return set;
+}
+
+function sumOfInt() {
+  const abundNums = fillSet();
+  let sum = 0;
+  for (let i = limit; i > 0; i--) {
+    let hasPair = false;
+    for (const num of abundNums) {
+      if (abundNums.has(i - num)) {
+        hasPair = true;
+        break;
+      }
+    }
+    if (!hasPair) sum += i;
+  }
+  return sum;
+}
+
+module.exports = { isAbundant, sumOfInt };
