1d0df09850296afaac503a9161f80aa1fb5b7055
Sergei Cherniaev <sergei@cherniaev.com>
2019-04-26 18:20:06 +0300

    test fixed

--- problem-24.test.js
+++ problem-24.test.js
@@ -3,6 +3,6 @@ const { permutations } = require("./problem-24");
 describe("Tests for problem 24: Lexicographic permutations", () => {
   /* eslint max-len: 0 */
   test("returns lexicographic permutation of a given sequence on certain position", () => {
-    expect(permutations([0, 1, 2], 5)).toBe(201);
+    expect(permutations([0, 1, 2], 5)).toEqual([2, 0, 1]);
   });
 });
