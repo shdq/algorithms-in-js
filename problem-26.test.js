8fcb6c4be1a2d6ddf3df202309c612106623d9a0
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-03 14:13:54 +0300

    test for no recurring cycle in the result of the expression 1/n

--- problem-26.test.js
+++ problem-26.test.js
@@ -5,4 +5,8 @@ describe("Tests for problem 26: Reciprocal cycles", () => {
   test("returns number of digits in recurring cycle of expression 1/n", () => {
     expect(getNumOfDigits(7)).toBe(6);
   });
+
+  test("returns false if there is no recurring cycle in the expression 1/n", () => {
+    expect(getNumOfDigits(5)).toBe(false);
+  });
 });
