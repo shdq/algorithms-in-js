02f1504c5e1b589611b0cacc69a26d65fed34408
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-04 20:32:51 +0300

    test for certain use case added

--- problem-26.test.js
+++ problem-26.test.js
@@ -6,6 +6,10 @@ describe("Tests for problem 26: Reciprocal cycles", () => {
     expect(getNumOfDigits(7)).toBe(6);
   });
 
+  test("returns number of digits in cycle even if the cycle starts after other digits like 0.1(6)", () => {
+    expect(getNumOfDigits(6)).toBe(1);
+  });
+
   test("returns false if there is no recurring cycle in the expression 1/n", () => {
     expect(getNumOfDigits(5)).toBe(false);
   });
