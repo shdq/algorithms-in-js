4268fc12ffd8a5b36ea37ee275e114cbc7d95d6d
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-09 22:49:10 +0300

    test file init

---
+++ problem-27.test.js
@@ -0,0 +1,7 @@
+const getProduct = require("./problem-27");
+
+describe("Tests for problem 27: Quadratic primes", () => {
+  test("empty function returns undefined", () => {
+    expect(getProduct()).toBe(undefined);
+  });
+});
