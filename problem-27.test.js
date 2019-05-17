cb07a93f0eb1c9a8091f41a9ba54c232dbb71dea
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-17 11:06:13 +0300

    test for counting consecutive primes function added

--- problem-27.test.js
+++ problem-27.test.js
@@ -1,6 +1,11 @@
-const getProduct = require("./problem-27");
+const { countPrimes, getProduct } = require("./problem-27");
 
+/* eslint max-len: 0 */
 describe("Tests for problem 27: Quadratic primes", () => {
+  test("returns amount of consecutive primes for a given variables for the quadratics formula", () => {
+    expect(countPrimes(-79, 1601)).toBe(80);
+  });
+
   test("empty function returns undefined", () => {
     expect(getProduct()).toBe(undefined);
   });
