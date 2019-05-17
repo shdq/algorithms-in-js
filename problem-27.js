d1dd8a34e8fc96be9e76d11a940119dfba14e06b
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-17 11:20:30 +0300

    solution for problem 27

--- problem-27.js
+++ problem-27.js
@@ -11,7 +11,18 @@ function countPrimes(a, b) {
 }
 
 function getProduct() {
-  return;
+  let max = 0;
+  let product;
+  for (let a = -999; a < 1000; a++) {
+    for (let b = -1000; b <= 1000; b++) {
+      const amount = countPrimes(a, b);
+      if (max < amount) {
+        max = amount;
+        product = a * b;
+      }
+    }
+  }
+  return product;
 }
 
 module.exports = { countPrimes, getProduct };
