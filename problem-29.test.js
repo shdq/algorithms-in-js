534b818359b3b14c864be859e5a86ed80c908f93
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-21 17:58:07 +0300

    test for the utility pow function that works with BigInt

--- problem-29.test.js
+++ problem-29.test.js
@@ -1,8 +1,12 @@
-const { distinctPow } = require("./problem-29");
+const { bigIntPow, distinctPow } = require("./problem-29");
 
 /* eslint max-len: 0 */
 describe("Tests for problem 29: Distinct powers", () => {
+  test("returns pow for BigInt base number", () => {
+    expect(bigIntPow(7n, 3)).toBe(343n);
+  });
+
   test("returns amount of unique numbers in sequence up to a given number/power", () => {
-    expect(distinctPow(5)).toBe(15);
+    expect(distinctPow(100)).toBe(9183);
   });
 });
