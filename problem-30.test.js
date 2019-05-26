ea7ce7629f9de2495996fc54de67b923a832def1
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-26 21:23:40 +0300

    test for the problem 30 added

--- problem-30.test.js
+++ problem-30.test.js
@@ -1,8 +1,11 @@
-const { isSumOfPowers } = require("./problem-30");
+const { isSumOfPowers, findNumbers } = require("./problem-30");
 
 /* eslint max-len: 0 */
 describe("Tests for problem 30: Digit fifth powers", () => {
   test("returns true for the numbers that can be written as the sum of powers of their digits", () => {
     expect(isSumOfPowers(8208, 4)).toBe(true);
+  });
+  test("returns the sum of all numbers that can be written as the sum of powers of their digits", () => {
+    expect(findNumbers(4)).toBe(19316);
   });
 });
