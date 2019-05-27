9981cfe69cc611f2ce76e2073a924fbbfb934e5a
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-27 10:54:26 +0300

    problem solved

--- problem-30.js
+++ problem-30.js
@@ -9,4 +9,12 @@ function isSumOfPowers(num, pow) {
   );
 }
 
-module.exports = { isSumOfPowers };
+function findNumbers(pow) {
+  const numbers = [];
+  for (let i = Math.pow(9, pow) * (pow + 1); i > 1; i--) {
+    if (isSumOfPowers(i, pow)) numbers.push(i);
+  }
+  return numbers.reduce((acc, cur) => acc + cur);
+}
+
+module.exports = { isSumOfPowers, findNumbers };
