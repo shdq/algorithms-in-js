7f82fa0643bff6d0623521d253ecd57ee52d70f1
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-01 21:33:49 +0300

    initial test

---
+++ problem-32.test.js
@@ -0,0 +1,8 @@
+const { product } = require("./problem-32");
+
+/* eslint max-len: 0 */
+describe("Tests for problem 32: Pandigital products", () => {
+  test("initial test", () => {
+    expect(product()).toBe(undefined);
+  });
+});
