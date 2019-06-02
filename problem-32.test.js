f72b6ac267a0f689e33bf5cf01d1240f412911a3
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-02 21:29:12 +0300

    test for the isPandigital function added

--- problem-32.test.js
+++ problem-32.test.js
@@ -1,7 +1,12 @@
-const { product } = require("./problem-32");
+const { product, isPandigital } = require("./problem-32");
 
 /* eslint max-len: 0 */
 describe("Tests for problem 32: Pandigital products", () => {
+  
+  test("returns true if given product is pandigital", () => {
+    expect(isPandigital(7254)).toBe(true);
+  });
+
   test("initial test", () => {
     expect(product()).toBe(undefined);
   });
