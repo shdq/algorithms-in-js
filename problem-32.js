22c42e9fd1a3f9b7a0cd8b0ed1c8ec90545f494d
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-03 23:46:39 +0300

    problem solved

--- problem-32.js
+++ problem-32.js
@@ -1,7 +1,20 @@
 function isPandigital(str) {
   const set = new Set(str.split(""));
-
   return set.size === 9 && !set.has("0");
+}
+
+function product() {
+  const products = new Set();
+  for (let i = 2; i < 100; i++) {
+    let j;
+    for (i < 10 ? (j = 1234) : (j = 123); j < 10000 / i; j++) {
+      const product = i * j;
+      if (isPandigital(i.toString() + j.toString() + product.toString())) {
+        products.add(product);
+      }
+    }
+  }
+  return [...products].reduce((acc, cur) => acc + cur);
 }
 
 module.exports = { product, isPandigital };
