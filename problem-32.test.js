7f941aff4b28e5f570b3a02f1049832bf7501b9a
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-03 23:47:40 +0300

    test input fixed

--- problem-32.test.js
+++ problem-32.test.js
@@ -3,7 +3,7 @@ const { product, isPandigital } = require("./problem-32");
 /* eslint max-len: 0 */
 describe("Tests for problem 32: Pandigital products", () => {
   test("returns true if given product is pandigital", () => {
-    expect(isPandigital(7254)).toBe(true);
+    expect(isPandigital("348956127")).toBe(true);
   });
 
   test("returns the sum of all pandigital products", () => {
