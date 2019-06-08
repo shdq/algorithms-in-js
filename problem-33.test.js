8bb8a8348c42e11850901668255df9533bf8c856
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-08 22:15:40 +0300

    test adjusted

--- problem-33.test.js
+++ problem-33.test.js
@@ -3,6 +3,6 @@ const { findFractions } = require("./problem-33");
 /* eslint max-len: 0 */
 describe("Tests for the problem 33: Digit canceling fractions", () => {
   test("returns all the non-trivial fractions containing two digits in the numerator and denominator", () => {
-    expect(findFractions()).toBe(4);
+    expect(findFractions().length).toBe(4);
   });
 });
