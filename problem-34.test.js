ddff6f052ea3676381e3160aacdd4bec00f259ad
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-11 19:12:28 +0300

    test for the problem solution

--- problem-34.test.js
+++ problem-34.test.js
@@ -1,8 +1,12 @@
-const { serializeFactorials } = require("./problem-34");
+const { serializeFactorials, solution } = require("./problem-34");
 
 /* eslint max-len: 0 */
 describe("Tests for the problem 34: Digit factorials", () => {
   test("returns value of 8! from serialized array of factorials", () => {
     expect(serializeFactorials()[8]).toBe(40320);
+  });
+
+  test("returns the sum of all numbers which are equal to the sum of the factorial of their digits", () => {
+    expect(solution()).toBe(40730);
   });
 });
