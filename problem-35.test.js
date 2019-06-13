b52c9189a6b93434b9cdb9152b6da98289ee786e
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-13 12:54:31 +0300

    test for the generatePrimes function

--- problem-35.test.js
+++ problem-35.test.js
@@ -1,9 +1,12 @@
-const { rotate } = require("./problem-35");
+const { rotate, generatePrimes } = require("./problem-35");
 
 /* eslint max-len: 0 */
 describe("Tests for the problem 35: Circular primes", () => {
   test("returns rotated string", () => {
     expect(rotate("197")).toBe("971");
     expect(rotate("abc")).toBe("bca");
+  });
+  test("returns primes up to a given limit", () => {
+    expect(generatePrimes(100).length).toBe(25);
   });
 });
