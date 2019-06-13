e4fb41042ae2e612611e834111c05243fc8d4e17
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-13 12:58:41 +0300

    function that generates primes up to limit added

--- problem-35.js
+++ problem-35.js
@@ -1,5 +1,17 @@
+const { isPrime } = require("./problem-7");
+
 function rotate(str) {
   return str.slice(1) + str[0];
 }
 
-module.exports = { rotate };
+function generatePrimes(limit = 100) {
+  const primes = [];
+  let n = 2;
+  while (n <= limit) {
+    if (isPrime(n)) primes.push(n);
+    n++;
+  }
+  return primes;
+}
+
+module.exports = { rotate, generatePrimes };
