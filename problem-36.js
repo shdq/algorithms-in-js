067363860f3de44b29f6ec94eaa1645c3964add1
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-15 21:25:57 +0300

    problem solved

--- problem-36.js
+++ problem-36.js
@@ -9,4 +9,12 @@ function isPalindrom(str) {
   );
 }
 
-module.exports = { isPalindrom };
+function solution() {
+  let sum = 0;
+  for (let i = 1; i < 1000000; i++) {
+    if (isPalindrom(i.toString()) && isPalindrom(i.toString(2))) sum += i;
+  }
+  return sum;
+}
+
+module.exports = { isPalindrom, solution };
