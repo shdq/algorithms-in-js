5ca25f940dfb5dc2f90f9422bdcfa6daa15aaf54
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-28 22:41:03 +0300

    test for the problem

--- problem-42.test.js
+++ problem-42.test.js
@@ -1,8 +1,15 @@
-const { isTriangle } = require("./problem-42");
+const fs = require("fs");
+const words = fs.readFileSync("problem-42-data.txt", "utf-8");
+
+const { isTriangle, solution } = require("./problem-42");
 
 /* eslint max-len: 0 */
 describe("Tests for the problem 42: Coded triangle numbers", () => {
   test("returns true if a given number is triangle number", () => {
     expect(isTriangle(55)).toBe(true);
+  });
+
+  test("returns true if a given number is triangle number", () => {
+    expect(solution(words)).toBe(162);
   });
 });
