d41608c706707627f09928d1535f225669fe5587
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-28 22:41:44 +0300

    problem solved

--- problem-42.js
+++ problem-42.js
@@ -1,6 +1,17 @@
-function isTriangle(n) {
-  // inverted formula t = (n * (n + 1)) / 2
-  return (Math.sqrt(1 + 8 * n) - 1) / 2 % 1 === 0;
+const { alphaValue } = require("./problem-22");
+
+function solution(words) {
+  const arr = words.replace(/"/g, "").split(",");
+  let count = 0;
+  arr.forEach(word => {
+    if (isTriangle(alphaValue(word))) count++;
+  });
+  return count;
 }
 
-module.exports = { isTriangle };
+function isTriangle(n) {
+  // inverted formula t = (n * (n + 1)) / 2
+  return ((Math.sqrt(1 + 8 * n) - 1) / 2) % 1 === 0;
+}
+
+module.exports = { isTriangle, solution };
