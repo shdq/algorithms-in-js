3557bb83b663f7b299372f8c5626439fcaa026dc
Sergei Cherniaev <sergei@cherniaev.com>
2019-06-29 21:55:29 +0300

    minor fix

--- problem-22.js
+++ problem-22.js
@@ -1,7 +1,6 @@
 function alphaValue(word) {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return word.split("").reduce((acc, value) => {
-    // if (typeof acc === "string") acc = alphabet.indexOf(acc) + 1;
     return acc + alphabet.indexOf(value) + 1;
   }, 0);
 }
