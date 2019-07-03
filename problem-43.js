361644e86b86b56420e44105fdcd813e2a37eb6e
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-03 11:15:56 +0300

    functions refactored to be clear and pure

--- problem-43.js
+++ problem-43.js
@@ -1,12 +1,8 @@
 function multiplesOf(num) {
   const result = [];
-  for (let i = 999; i >= 100; i--) {
-    if (i % num === 0 && new Set(i.toString().split("")).size === 3) {
-      result.push(i.toString());
-    }
-  }
-  for (let i = 99; i >= 10; i--) {
-    const str = "0" + i.toString();
+  for (let i = 10; i <= 999; i++) {
+    let str = i.toString();
+    if (i < 100) str = "0" + str;
     if (i % num === 0 && new Set(str.split("")).size === 3) {
       result.push(str);
     }
@@ -14,29 +10,28 @@ function multiplesOf(num) {
   return result;
 }
 
-const m17 = multiplesOf(17);
-
-function solution(candidates = m17, primes = [1, 2, 3, 5, 7, 11, 13]) {
+function solution(
+  candidates = multiplesOf(17),
+  primes = [1, 2, 3, 5, 7, 11, 13]
+) {
   if (candidates[0].length === 10) {
     return candidates.reduce((acc, cur) => acc + Number(cur), 0);
   }
-  let str = candidates.pop();
-  const prime = primes.pop();
-  const result = [];
-  while (str) {
-    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(
-      el => str.indexOf(el) < 0
-    );
 
-    for (const el of arr) {
-      if (Number(el.toString() + str.slice(0, 2)) % prime === 0) {
-        result.push(el.toString() + str);
+  const reducedCandidates = [];
+  const prime = primes.slice(-1);
+
+  for (const candidate of candidates) {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(
+      num => candidate.indexOf(num) < 0
+    );
+    for (const num of arr) {
+      if (Number(num.toString() + candidate.slice(0, 2)) % prime === 0) {
+        reducedCandidates.push(num.toString() + candidate);
       }
     }
-
-    str = candidates.pop();
   }
-  return solution(result, primes);
+  return solution(reducedCandidates, primes.slice(0, -1));
 }
 
-module.exports = solution;
+module.exports = { multiplesOf, solution };
