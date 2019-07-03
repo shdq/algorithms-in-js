98bc9489468dfe809eb9269b57b6ea10bde05092
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-03 12:07:15 +0300

    problem 27 optimized by time

--- problem-27.js
+++ problem-27.js
@@ -1,4 +1,6 @@
 const { isPrime } = require("./problem-7");
+const sieve = require("./sieve");
+const primes = sieve(1000);
 
 function countPrimes(a, b) {
   let n = -1;
@@ -14,7 +16,8 @@ function getProduct() {
   let max = 0;
   let product;
   for (let a = -999; a < 1000; a++) {
-    for (let b = -1000; b <= 1000; b++) {
+    for (const b of primes) {
+      if (!isPrime(a + b + 1)) continue;
       const amount = countPrimes(a, b);
       if (max < amount) {
         max = amount;
