7db27bd6ae3c9461ae35a537564f561c92071985
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-05 16:50:18 +0300

    optimized solution added

--- problem-44.js
+++ problem-44.js
@@ -6,4 +6,18 @@ function genPentagonal(amount) {
   return numbers;
 }
 
-module.exports = { genPentagonal };
+function solution(numbers = genPentagonal(2200)) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j <= numbers.length; j++) {
+      const p = numbers[i] + numbers[j];
+      if (((1 + Math.sqrt(1 + 24 * p)) / 6) % 1 === 0) {
+        const d = Math.abs(numbers[i] - numbers[j]);
+        if (((1 + Math.sqrt(1 + 24 * d)) / 6) % 1 === 0) {
+          return d;
+        }
+      }
+    }
+  }
+}
+
+module.exports = { genPentagonal, solution };
