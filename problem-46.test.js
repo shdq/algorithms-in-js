54fa904c7e54cdb0a3bb603d1809bdb33d08328c
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-08 11:26:40 +0300

    test for the problem

--- problem-46.test.js
+++ problem-46.test.js
@@ -1,8 +1,13 @@
-const { isTwiceSquare } = require("./problem-46");
+const { isTwiceSquare, solution } = require("./problem-46");
+const sieve = require("./sieve");
 
 /* eslint max-len: 0 */
 describe("Tests for the problem 45: Triangular, pentagonal, and hexagonal", () => {
   test("returns true if square root of the number divided by 2 is integer", () => {
     expect(isTwiceSquare(18)).toBe(true);
+  });
+
+  test("returns true if square root of the number divided by 2 is integer", () => {
+    expect(solution(sieve(6000))).toBe(5777);
   });
 });
