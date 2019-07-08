fca801c891794aa4f66b81aebec15fa9445cab54
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-08 11:23:25 +0300

    test fixed

--- problem-46.test.js
+++ problem-46.test.js
@@ -2,7 +2,7 @@ const { isTwiceSquare } = require("./problem-46");
 
 /* eslint max-len: 0 */
 describe("Tests for the problem 45: Triangular, pentagonal, and hexagonal", () => {
-  test("returns true if twice square of the number is integer", () => {
-    expect(isTwiceSquare(4)).toBe(true);
+  test("returns true if square root of the number divided by 2 is integer", () => {
+    expect(isTwiceSquare(18)).toBe(true);
   });
 });
