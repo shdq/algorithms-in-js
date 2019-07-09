9a470a3b38194fb732f5c47db2c04babd6866232
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-09 14:29:28 +0300

    description of test changed

--- problem-46.test.js
+++ problem-46.test.js
@@ -7,7 +7,7 @@ describe("Tests for the problem 46: Goldbach's other conjecture", () => {
     expect(isTwiceSquare(18)).toBe(true);
   });
 
-  test("returns true if square root of the number divided by 2 is integer", () => {
+  test("returns the smallest odd composite that cannot be written as the sum of a prime and twice a square", () => {
     expect(solution(sieve(6000))).toBe(5777);
   });
 });
