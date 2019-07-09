2a76f678eac0936b79489707392f9fa0d3d6f7b3
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-09 14:27:44 +0300

    description of test file changed

--- problem-46.test.js
+++ problem-46.test.js
@@ -2,7 +2,7 @@ const { isTwiceSquare, solution } = require("./problem-46");
 const sieve = require("./sieve");
 
 /* eslint max-len: 0 */
-describe("Tests for the problem 45: Triangular, pentagonal, and hexagonal", () => {
+describe("Tests for the problem 46: Goldbach's other conjecture", () => {
   test("returns true if square root of the number divided by 2 is integer", () => {
     expect(isTwiceSquare(18)).toBe(true);
   });
