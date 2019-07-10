6420f7e7eafc82f96197b20071f5de2136b5cfaa
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-10 14:27:54 +0300

    test for the problem

--- problem-47.test.js
+++ problem-47.test.js
@@ -1,11 +1,14 @@
-const { numOfPrimeFactors } = require("./problem-47");
+const { numOfPrimeFactors, solution } = require("./problem-47");
 const sieve = require("./sieve");
-
 const primes = sieve(100000);
 
 /* eslint max-len: 0 */
 describe("Tests for the problem 47: Distinct primes factors", () => {
   test("returns number of prime factors for a given number", () => {
     expect(numOfPrimeFactors(644, primes)).toBe(3);
+  });
+
+  test("returns the first of four consecutive integers that have four prime factors", () => {
+    expect(solution(primes)).toBe(134043);
   });
 });
