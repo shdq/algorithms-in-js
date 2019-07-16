48f112ff7683d9e9b30c4b84b2fb04b8eeeb0c05
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-16 22:36:38 +0300

    half of the solution

--- problem-49.js
+++ problem-49.js
@@ -1,5 +1,16 @@
-function solution() {
-  return;
+function solution(primes) {
+  for (let i = 0; i < primes.length; i++) {
+    for (let j = i + 1; j < primes.length; j++) {
+      if (primes.indexOf(primes[j] - primes[i] + primes[j]) >= 0) {
+        const one = primes[i].toString();
+        const two = primes[j].toString();
+        const three = (primes[j] - primes[i] + primes[j]).toString();
+
+        /* if one two and three are permutataions of each other */
+        return one + two + three;
+      }
+    }
+  }
 }
 
 module.exports = { solution };
