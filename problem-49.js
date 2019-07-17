cc98e756c9f8e6da981c03407f9b0b0698b92ba0
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-17 11:42:37 +0300

    problem solved

--- problem-49.js
+++ problem-49.js
@@ -1,13 +1,27 @@
+function sortInt(n) {
+  return Number(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => a - b)
+      .join("")
+  );
+}
+
 function solution(primes) {
   for (let i = 0; i < primes.length; i++) {
     for (let j = i + 1; j < primes.length; j++) {
       if (primes.indexOf(primes[j] - primes[i] + primes[j]) >= 0) {
-        const one = primes[i].toString();
-        const two = primes[j].toString();
-        const three = (primes[j] - primes[i] + primes[j]).toString();
+        const first = primes[i].toString();
+        const second = primes[j].toString();
+        const third = (primes[j] - primes[i] + primes[j]).toString();
 
-        /* if one two and three are permutataions of each other */
-        return one + two + three;
+        if (
+          sortInt(first) === sortInt(second) &&
+          sortInt(first) === sortInt(third)
+        ) {
+          return first + second + third;
+        }
       }
     }
   }
