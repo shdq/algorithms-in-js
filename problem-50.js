89322deb24d412cfffa22da42acaca9ab8432698
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-22 12:34:23 +0300

    comment added for future me and refactored

--- problem-50.js
+++ problem-50.js
@@ -2,23 +2,23 @@ const sieve = require("./sieve");
 
 function consPrimeSum(limit) {
   const primes = sieve(limit);
-  let amount = 0;
-  let max = 0;
-  let lastJ = primes.length - 1;
+  let maxSequence = 0;
+  let maxSum = 0;
   for (let i = 0; i < primes.length; i++) {
-    for (let j = i + amount; j <= lastJ; j++) {
+    /*
+    sequence less than we already have isn't interested for us,
+    thats why we start inner loop with j = i + amount
+    */
+    for (let j = i + maxSequence; j <= primes.length; j++) {
       const sum = primes.slice(i, j + 1).reduce((acc, val) => acc + val);
-      if (sum > limit) {
-        lastJ = j + 1;
-        break;
-      }
-      if (primes.indexOf(sum) > -1 && j - i > amount) {
-        amount = j - i;
-        max = sum;
+      if (sum > limit) break;
+      if (primes.indexOf(sum) > -1 && j - i > maxSequence) {
+        maxSequence = j - i;
+        maxSum = sum;
       }
     }
   }
-  return max;
+  return maxSum;
 }
 
 module.exports = { consPrimeSum };
