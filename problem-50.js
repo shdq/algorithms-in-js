83fd656b5a191207c7eb19e1233cf1761ddf9036
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-22 12:11:18 +0300

    bug fixed in problem 50, optimized by time and refactored

--- problem-50.js
+++ problem-50.js
@@ -4,15 +4,15 @@ function consPrimeSum(limit) {
   const primes = sieve(limit);
   let amount = 0;
   let max = 0;
-  let lastJ = primes.length;
-  for (let i = primes.length - 1; i >= 1; i--) {
+  let lastJ = primes.length - 1;
+  for (let i = 0; i < primes.length; i++) {
     for (let j = i + amount; j <= lastJ; j++) {
       const sum = primes.slice(i, j + 1).reduce((acc, val) => acc + val);
       if (sum > limit) {
         lastJ = j + 1;
         break;
       }
-      if (primes.indexOf(sum) > -1) {
+      if (primes.indexOf(sum) > -1 && j - i > amount) {
         amount = j - i;
         max = sum;
       }
