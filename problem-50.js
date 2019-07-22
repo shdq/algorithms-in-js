685074be65210efb537866884cc8f2939876f486
Sergei Cherniaev <sergei@cherniaev.com>
2019-07-22 11:26:53 +0300

    execution time drastically reduced

--- problem-50.js
+++ problem-50.js
@@ -2,25 +2,23 @@ const sieve = require("./sieve");
 
 function consPrimeSum(limit) {
   const primes = sieve(limit);
-  let result = 0;
   let amount = 0;
+  let max = 0;
+  let lastJ = primes.length;
   for (let i = primes.length - 1; i >= 1; i--) {
-    for (let j = 0; j < i; j++) {
-      let sum = 0;
-      let k = j;
-      while (true) {
-        sum += primes[k];
-        if (sum > primes[i]) break;
-        if (sum === primes[i] && amount < k-j) {
-          result = sum;
-          amount = k - j;
-          break;
-        }
-        k++;
+    for (let j = i + amount; j <= lastJ; j++) {
+      const sum = primes.slice(i, j + 1).reduce((acc, val) => acc + val);
+      if (sum > limit) {
+        lastJ = j + 1;
+        break;
+      }
+      if (primes.indexOf(sum) > -1) {
+        amount = j - i;
+        max = sum;
       }
     }
   }
-  return result;
+  return max;
 }
 
 module.exports = { consPrimeSum };
